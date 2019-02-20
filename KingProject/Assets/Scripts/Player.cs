655661ef6d4fa6384879f78201e7a1c804f54e6d
Evandro Assis <evandro.assis@hotmail.com>
2019-02-20 16:28:32 -0400

    Script Player

--- KingProject/Assets/Scripts/Player.cs
+++ KingProject/Assets/Scripts/Player.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed;
+    public int jumpForce;
+    public int health;
+    public Transform groundCheck;
+
+    private bool invunerable = false;
+    private bool grounded = false;
+    private bool jumping = false;
+    private bool facingRight = true;
+
+    private SpriteRenderer sprite;
+    private Rigidbody2D rb2d;
+    private Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        sprite  = GetComponent<SpriteRenderer>();
+        rb2d    = GetComponent<Rigidbody2D>();
+        anim    = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+        if (Input.GetButtonDown("Jump") && grounded) {
+            jumping = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+        rb2d.velocity = new Vector2(move * speed, rb2d.velocity.y);
+
+        if ((move < 0f && facingRight) || (move > 0f && !facingRight)) {
+            Flip();
+        }
+
+        if (jumping) {
+            rb2d.AddForce(new Vector2(0f, jumpForce));
+            jumping = false;
+        }
+    }
+
+    void Flip() {
+        facingRight = !facingRight;
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
     }
 }
